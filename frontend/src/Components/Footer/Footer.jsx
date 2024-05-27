b79ffc330d9ad1bcadcfef26f1eb95e2a0d5b348
Jhon_camargo <jhonfonseca324@gmail.com>
2024-05-27 03:04:12 -0500

    URL manual de usuario

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -8,34 +8,43 @@ import image4 from "../../Assets/f4.png";
 const Footer = () => {
   return (
     <footer>
-      <div className="footer-column">
-        <div
-          className="footer-icon"
-          style={{ backgroundImage: `url(${image1})` }}
-        ></div>
-        <p className="footer-text">Elige la ubicación de tu oferta.</p>
+      <div className="footer-columns">
+        <div className="footer-column">
+          <div
+            className="footer-icon"
+            style={{ backgroundImage: `url(${image1})` }}
+          ></div>
+          <p className="footer-text">Elige la ubicación de tu oferta.</p>
+        </div>
+        <div className="footer-column">
+          <div 
+            className="footer-icon"
+            style={{ backgroundImage: `url(${image2})` }}
+          ></div>
+          <p className="footer-text">Completa tu perfil para destacar ante otros postulantes.</p>
+        </div>
+        <div className="footer-column">
+          <div 
+            className="footer-icon"
+            style={{ backgroundImage: `url(${image3})` }}
+          ></div>
+          <p className="footer-text">Postúlate a los anuncios que sean aptos para tu perfil.</p>
+        </div>
+        <div className="footer-column">
+          <div 
+            className="footer-icon"
+            style={{ backgroundImage: `url(${image4})` }}
+          ></div>
+          <p className="footer-text">Puedes elegir ofertas que te dan hospedaje.</p>
+        </div>
       </div>
-      <div className="footer-column">
-        <div 
-          className="footer-icon"
-          style={{ backgroundImage: `url(${image2})` }}
-        ></div>
-        <p className="footer-text">Completa tu perfil para destacar ante otros postulantes.</p>
-      </div>
-      <div className="footer-column">
-        <div 
-          className="footer-icon"
-          style={{backgroundImage: `url(${image3})` }}
-        ></div>
-        <p className="footer-text">Postúlate a los anuncios que sean aptos para tu perfil.</p>
-      </div>
-      <div className="footer-column">
-        <div 
-          className="footer-icon"
-          style={{backgroundImage: `url(${image4})` }}
-        ></div>
-        <p className="footer-text">Puedes elegir ofertas que te dan hospedaje.</p>
-      </div>
+      <a 
+        href="https://app.tango.us/app/workflow/Manual-de-Usuario--Campo-Conecta--4709fdfc2475489495cf82a55714db3b" 
+        className="footer-link"
+        target="_blank"
+      >
+        <u>Manual de usuario</u>
+      </a>
     </footer>
   );
 };
